4a88a776a803fd0bf38e6500c141acbf8cedc6cb
karim <k.guezlane@gmail.com>
2021-07-09 13:55:35 +0100

    ignoring geocoding API.

--- src/response_handling.js
+++ src/response_handling.js
@@ -1,12 +1,14 @@
 import { getWeatherResponse } from "./api_request";
-import { getPositionResponse } from "./api_request";
+//import { getPositionResponse } from "./api_request";
 import {ApiInformation} from "./api_request";
-const getLatLon= async function(location){
+// ignoring this until getting a better geocoding API
+
+/*const getLatLon= async function(location){
 	const response= await getPositionResponse(location);
 	let lat=response.results[0].locations[0].latLng.lat;
 	let lon=response.results[0].locations[0].latLng.lng;
 	return({lat,lon});
-};
+};*/
 const getLocation= function(){
 	let input= document.getElementById("location");
 	if(!input.value){return;}
@@ -15,15 +17,15 @@ const getLocation= function(){
 	return location;
 };
 const getWeatherInfo= async function(){
-	const latLng= await getLatLon(getLocation());
-	const info= await ApiInformation(latLng,"metric");
+	//const latLng= await getLatLon(getLocation());
+	const info= await ApiInformation(getLocation(),"metric");
 	return info;
 };
 const handleResponses= async function(){
 	const obj= await getWeatherInfo();
 	const weatherResponse= await getWeatherResponse(obj);
-	const locationResponse= await getPositionResponse(getLocation());
-	return{weatherResponse,locationResponse};
+	//const locationResponse= await getPositionResponse(getLocation());
+	return{weatherResponse};
 };
 
 export {handleResponses};

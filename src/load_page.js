3e70d01ef741d9c32017c624dd90f03965d97631
karim <k.guezlane@gmail.com>
2021-07-09 14:38:05 +0100

    importing modules

---
+++ src/load_page.js
@@ -0,0 +1,5 @@
+import { createMain } from "./load_main";
+import { renderMain } from "./load_main";
+import { createSideBar } from "./load_sideBar";
+import { renderSideBar } from "./load_sideBar";
+

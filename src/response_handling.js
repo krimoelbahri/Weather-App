5b2e9c0e86b55bdf413103a1f92bfca1e8a27110
karim <k.guezlane@gmail.com>
2021-07-09 11:16:40 +0100

    getting longitude and latitude of a location

--- src/response_handling.js
+++ src/response_handling.js
@@ -1,3 +1,11 @@
 import { getWeatherResponse } from "./api_request";
 import { getPositionResponse } from "./api_request";
 import {ApiInformation} from "./api_request";
+const getLatLon= async function(location){
+	const response= await getPositionResponse(location);
+	let lat=response.results[0].locations[0].latLng.lat;
+	let lon=response.results[0].locations[0].latLng.lng;
+	return({lat,lon});
+};
+
+

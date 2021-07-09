4ae284d28472b4ba7f5408bbd3049e9fc160fe29
karim <k.guezlane@gmail.com>
2021-07-09 14:40:09 +0100

    adding logic to load website

--- src/load_page.js
+++ src/load_page.js
@@ -3,3 +3,11 @@ import { renderMain } from "./load_main";
 import { createSideBar } from "./load_sideBar";
 import { renderSideBar } from "./load_sideBar";
 
+const loadSite = function(){
+	let container= document.getElementById("container");
+	container.appendChild(createMain());
+	container.appendChild(createSideBar());
+	let searchForm= document.getElementById("searchForm");
+	searchForm.addEventListener("submit",renderPage);
+};
+export {loadSite};
